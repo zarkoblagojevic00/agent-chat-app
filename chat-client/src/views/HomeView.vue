7d0a55734becdfce9552ca253e5e611eba08dd93
zarkoblagojevic00 <blagojevic.ra44.2018@uns.ac.rs>
2022-04-01 20:07:02 +0200

    Init commit - chat web client

---
+++ chat-client/src/views/HomeView.vue
@@ -0,0 +1,18 @@
+<template>
+    <div class="home">
+        <img alt="Vue logo" src="../assets/agent-chat-logo-256.svg" />
+        <HelloWorld msg="Welcome to Agent Chat App" />
+    </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import HelloWorld from "@/components/HelloWorld.vue";
+
+export default {
+    name: "HomeView",
+    components: {
+        HelloWorld,
+    },
+};
+</script>
