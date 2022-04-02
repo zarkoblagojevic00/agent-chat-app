a086d905d12abc21adab836e1d40a17cbe97489c
zarkoblagojevic00 <blagojevic.ra44.2018@uns.ac.rs>
2022-04-02 12:45:20 +0200

    feat: Added navbar for guest user

---
+++ chat-client/src/components/NavBar.vue
@@ -0,0 +1,77 @@
+<template>
+    <div class="container">
+        <div class="logo-container">
+            <img alt="Agent logo" src="../assets/agent-chat-logo-64.svg" />
+            <div class="app-name gradient-text">Agent Chat</div>
+        </div>
+        <nav>
+            <router-link to="/">Login</router-link>
+            <router-link
+                class="register-link gradient-bg ease-in"
+                to="/register"
+                >Register</router-link
+            >
+        </nav>
+    </div>
+</template>
+
+<script>
+export default {
+    name: "NavBar",
+};
+</script>
+
+<style scoped>
+.container {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    padding: 5px 10%;
+    background: var(--background);
+}
+
+.logo-container {
+    display: flex;
+    justify-content: flex-start;
+    align-items: center;
+}
+
+.app-name {
+    font-size: 1.8rem;
+}
+
+nav a {
+    text-decoration: none;
+    margin: 0 10px;
+    font-size: 1.05rem;
+    color: var(--white);
+}
+
+nav a.router-link-exact-active {
+    color: var(--blue);
+}
+
+.register-link {
+    padding: 0.5em 0.8em;
+    border-radius: 0.2em 0.7em;
+    color: var(--white);
+}
+
+.register-link:hover {
+    background-image: linear-gradient(
+        45deg,
+        var(--darker-blue),
+        var(--darker-purple)
+    );
+    background-size: 100%;
+}
+
+a.register-link.router-link-exact-active {
+    color: var(--white);
+    background-image: linear-gradient(
+        45deg,
+        var(--darker-blue),
+        var(--darker-purple)
+    );
+}
+</style>
--- chat-client/src/views/HomeView.vue
+++ chat-client/src/views/HomeView.vue
@@ -1,18 +1,11 @@
 <template>
-    <div class="home">
-        <img alt="Vue logo" src="../assets/agent-chat-logo-256.svg" />
-        <HelloWorld msg="Welcome to Agent Chat App" />
+    <div>
+        <img alt="Agent logo" src="../assets/agent-chat-logo-256.svg" />
     </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from "@/components/HelloWorld.vue";
-
 export default {
     name: "HomeView",
-    components: {
-        HelloWorld,
-    },
 };
 </script>
